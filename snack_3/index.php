03a5216de8cbf582e139c67d9135ea1868e24246
EmanueleBrusa <emanuelebrusa@hotmail.it>
2023-07-04 17:02:09 +0200

    snack 3

---
+++ snack_3/index.php
@@ -0,0 +1,66 @@
+<?php
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+foreach ($posts as $date => $postList) {
+    echo "Data: " .$date;
+    foreach($postList as $post){
+        echo "<br>";
+        echo "- Titolo: ".$post['title']. "<hr>";
+        echo "- Autore: ".$post['author']. "<hr>";
+        echo "- Testo: ".$post['text']. "<hr>";
+        echo "<br>";
+    }
+    echo "<br>";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>snack 3</title>
+</head>
+<body>
+    
+</body>
+</html>
